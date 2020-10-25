477c529a2d0dc8cae033980f3ca51fd09bdfd6a7
latermonk <sprout_wang@163.com>
2020-10-26 07:40:29 +0800

    Update class_demo01.py

--- class_demo01.py
+++ class_demo01.py
@@ -1,4 +1,13 @@
 
+class MyClass:
+    """一个简单的类实例"""
+    i = 12345
+    def f(self):
+        return 'hello world'
+
+
+
+
 
 
 class Bird:
@@ -22,5 +31,21 @@ def test():
 
 
 
+
+def test_02():
+
+    # 实例化类
+    x = MyClass()
+
+    # 访问类的属性和方法
+    print("MyClass 类的属性 i 为：", x.i)
+    print("MyClass 类的方法 f 输出为：", x.f())
+
+
+
+
+
 if __name__ == '__main__':
-    test()
+    # test()
+
+    test_02()
