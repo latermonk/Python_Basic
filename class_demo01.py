9b1d79dd45a7baece3388ac22f011a83bc482282
latermonk <sprout_wang@163.com>
2020-10-26 07:41:53 +0800

    Update class_demo01.py

--- class_demo01.py
+++ class_demo01.py
@@ -4,6 +4,7 @@ class MyClass:
     i = 12345
     def f(self):
         return 'hello world'
+    j = 987
 
 
 
@@ -39,6 +40,7 @@ def test_02():
 
     # 访问类的属性和方法
     print("MyClass 类的属性 i 为：", x.i)
+    print("yClass 类的属性 j 为：", x.j)
     print("MyClass 类的方法 f 输出为：", x.f())
 
 
