5f60487af21845c1083c7b5c726e6f2491a95081
latermonk <sprout_wang@163.com>
2020-10-25 20:37:06 +0800

    Update class_demo01.py

--- class_demo01.py
+++ class_demo01.py
@@ -1,6 +1,25 @@
 
 
 
+class Bird:
+    '这是学习python的第一个类'
+    eyes = "two"
+    def __init__(self, color,feet):  # '为python对象增                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 '
+        self.color = color
+        self.feet = feet
+    def call(self,cd):
+        print("This bird:",cd)
+
+
+
+def test():
+
+    print("Test !!! ")
+
+    xique = Bird("green", "two")
+    xique.call("gezhi")
+
+
 
 
 if __name__ == '__main__':
